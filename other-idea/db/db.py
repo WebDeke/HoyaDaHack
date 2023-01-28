d0e85ace538b5cf354f29f19775af4b4432e6c40
Arjun Sundaram <arjun.sundaram@inadev.com>
2023-01-28 09:20:08 -0500

    Changed db

--- other-idea/db/db.py
+++ other-idea/db/db.py
@@ -1,14 +1,15 @@
 #db.py
 import streamlit as st
-import pymongo
+from pymongo import MongoClient
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
+@st.experimental_singleton(suppress_st_warning=True)
 def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
+    return MongoClient("mongodb+srv://asundar:5zGDjohcbP4TApTH@studyusers.b1bzofp.mongodb.net/?retryWrites=true&w=majority")
 
-client = init_connection("mongodb+srv://studyusers.b1bzofp.mongodb.net/StudyUsers")
+# client = init_connection("mongodb+srv://studyusers.b1bzofp.mongodb.net/StudyUsers")
+client = init_connection()
 
 # Pull data from the collection.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
