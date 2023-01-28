e617713208e2b28f476d3e58ca84775746ac5508
WebDeke <guruweb10@gmail.com>
2023-01-28 04:32:48 -0800

    testing

--- new-test.py
+++ new-test.py
@@ -2,29 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Find a Study Buddy Near You!')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+options = st.multiselect(
+    'What classes are you taking',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    )
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
-
-st.subheader('Number of pickups by hour')
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
+space = st.select_slider(
+    'Select your favorite space',
+    options=['Place A', 'Place B', 'Place C', 'Place D', 'Place E']
+    )
+st.write('I am taking', options, 'and my favorite study space is', space)
