68f2598de8818498ec691fdba5abb0ac77e616d2
Arjun Sundaram <arjun.sundaram@inadev.com>
2023-01-28 14:10:12 -0500

    Uncommented pymongo

--- db.py
+++ db.py
@@ -1,30 +1,58 @@
 
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
+# from pprint import pprint
+# import streamlit as st
+# from gsheetsdb import connect
+
+# rowcount = 3
+# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+#          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+# credentials = ServiceAccountCredentials.from_json_keyfile_name(
+#     "./studybuddy-376115-369daa1f5b57.json", scope)
+
+# client = gspread.authorize(credentials)
+
+
+# # Open the spreadhseet
+# sheet = client.open("Commilito Backend").worksheet("commilito_backend")
+
+# # Get a list of all records
+# data = sheet.get_all_records()
+# pprint(data)
+
+# public_gsheets_url = "https://docs.google.com/spreadsheets/d/1tYPHXibwW4lMQBH8iaXOXqZHg5P7-lVQ7JZ8EGmki5w/edit#gid=0"
+# # Create a connection object.
+# conn = connect()
+
+#db.py
 import streamlit as st
-from gsheetsdb import connect
+from pymongo import MongoClient
 
-rowcount = 3
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton(suppress_st_warning=True)
+def init_connection():
+    return MongoClient("mongodb+srv://asundar:5zGDjohcbP4TApTH@studyusers.b1bzofp.mongodb.net/?retryWrites=true&w=majority")
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    "./studybuddy-376115-369daa1f5b57.json", scope)
+# client = init_connection("mongodb+srv://studyusers.b1bzofp.mongodb.net/StudyUsers")
+client = init_connection()
 
-client = gspread.authorize(credentials)
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def get_data():
+    db = client.Users
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.experimental_memo
+    return items
 
+items = get_data()
 
-# Open the spreadhseet
-sheet = client.open("Commilito Backend").worksheet("commilito_backend")
-
-# Get a list of all records
-data = sheet.get_all_records()
-pprint(data)
-
-public_gsheets_url = "https://docs.google.com/spreadsheets/d/1tYPHXibwW4lMQBH8iaXOXqZHg5P7-lVQ7JZ8EGmki5w/edit#gid=0"
-# Create a connection object.
-conn = connect()
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['pet']}:")
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
