6d752c7db009b105457fb0973e8f5d1012be7b7a
WebDeke <guruweb10@gmail.com>
2023-01-28 07:54:07 -0800

    shortened class names

--- new-app.py
+++ new-app.py
@@ -6,11 +6,12 @@ st.title('Commilito: Find your study partner in arms.')
 
 classes = st.multiselect(
     'What classes are you taking',
-    ['COSC 225 - Computer Networks', 'MATH 150 – Linear Algebra', 'CHEM 228 - Synthetic Methods', 'PHYS 101 - Physics', 'HIST 007 – Introduction to Early History', 'CULP 346 - Crit Geography: Theory and Practice', 'CLSS 141 - Roman History', 'CLSL 002 - Latin II'],
+    ['COSC 225', 'MATH 150', 'CHEM 228', 'PHYS 101', 'HIST 007', 'CULP 346', 'CLSS 141', 'CLSL 002'],
     )
 
 space = st.select_slider(
     'Select your favorite space',
     options=['Place A', 'Place B', 'Place C', 'Place D', 'Place E']
     )
-st.write('I am taking', classes, 'and my favorite study space is', space)
+
+if st.button("Find a partner"):
