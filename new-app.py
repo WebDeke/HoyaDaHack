6deb6efea41873de9a0035f5625b155f719364b0
WebDeke <guruweb10@gmail.com>
2023-01-28 08:49:55 -0800

    design changes

--- new-app.py
+++ new-app.py
@@ -3,10 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.title('Commilito: Find your study partner in arms.')
+col1, col2 = st.columns([3, 1])
 
-fname = st.text_input('First name')
-lname = st.text_input('Last name')
-
+col1.subheader("A wide column with a chart")
 classes = st.multiselect(
     'What classes are you taking',
     ['COSC 225', 'MATH 150', 'CHEM 228', 'PHYS 101', 'HIST 007', 'CULP 346', 'CLSS 141', 'CLSL 002'],
@@ -16,7 +15,9 @@ space = st.select_slider(
     'Select your favorite space',
     options=['Place A', 'Place B', 'Place C', 'Place D', 'Place E']
     )
-
-phone = st.text_input("Phone number")
+col2.subheader("A narrow column with the data")
+col2.fname = st.text_input('First name')
+col2.lname = st.text_input('Last name')
+col2.phone = st.text_input("Phone number")
 
 st.button("Find a partner")
