b040f16f7fcf4ece05970d1f218eae6448efb48d
WebDeke <guruweb10@gmail.com>
2023-01-28 01:39:00 -0800

    openai added

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import openai
-import streamlit as st
+openai.api_key = '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'
+
+model = 'text-davinci-003'
+
+response = openai.Completion.create(
+    prompt="How big is the moon?",
+    model = model
+)
