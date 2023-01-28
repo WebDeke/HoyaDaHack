c67da3360dcd26d1365a6f5c3c2bd321123556a0
WebDeke <guruweb10@gmail.com>
2023-01-28 08:53:53 -0800

    s

--- new-app.py
+++ new-app.py
@@ -2,22 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Commilito: Find your study partner in arms.')
-col1, col2 = st.columns([3, 1])
-
-col1.subheader("A wide column with a chart")
-col1.classes = st.multiselect(
+st.title('Commilito')
+st.subheader("Combine academic weapons.")
+classes = st.multiselect(
     'What classes are you taking',
     ['COSC 225', 'MATH 150', 'CHEM 228', 'PHYS 101', 'HIST 007', 'CULP 346', 'CLSS 141', 'CLSL 002'],
     )
 
-col1.space = st.select_slider(
+space = st.select_slider(
     'Select your favorite space',
     options=['Place A', 'Place B', 'Place C', 'Place D', 'Place E']
     )
-col2.subheader("A narrow column with the data")
-col2.fname = st.text_input('First name')
-col2.lname = st.text_input('Last name')
-col2.phone = st.text_input("Phone number")
+fname = st.text_input('First name')
+lname = st.text_input('Last name')
+phone = st.text_input("Phone number")
 
 st.button("Find a partner")
