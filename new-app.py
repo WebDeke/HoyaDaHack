f8200f418c8159f1a2a720ede962e6d9b8fff0f5
WebDeke <guruweb10@gmail.com>
2023-01-28 08:13:58 -0800

    f and l names

--- new-app.py
+++ new-app.py
@@ -4,6 +4,9 @@ import numpy as np
 
 st.title('Commilito: Find your study partner in arms.')
 
+fname = st.text_input('First name')
+lname = st.text_input('Last name')
+
 classes = st.multiselect(
     'What classes are you taking',
     ['COSC 225', 'MATH 150', 'CHEM 228', 'PHYS 101', 'HIST 007', 'CULP 346', 'CLSS 141', 'CLSL 002'],
