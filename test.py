af3f4dac1df35aaabbee58ba82da6819dfac11e0
WebDeke <guruweb10@gmail.com>
2023-01-28 02:08:19 -0800

    removing tings

--- test.py
+++
@@ -1,9 +0,0 @@
-import openai
-openai.api_key = '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'
-
-model = 'text-davinci-003'
-
-response = openai.Completion.create(
-    prompt="How big is the moon?",
-    model = model
-)
