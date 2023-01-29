7412f70e4bd756d2c60978cb1db935776db10334
Arjun Sundaram <arjun.sundaram@inadev.com>
2023-01-28 20:33:35 -0500

    More changes to app.py

--- new-app.py
+++ new-app.py
@@ -6,66 +6,7 @@ from pprint import pprint
 # from urllib import requests 
 # import http.client
 
-# import sys, subprocess
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gspread'])
-# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-#          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
-# credentials = ServiceAccountCredentials.from_json_keyfile_name(
-#     "./studybuddy-376115-369daa1f5b57.json", scope)
-
-# client = gspread.authorize(credentials)
-
 # httpConn = http.client.HTTPSConnection("localhost", 5003)
-
-
-# Open the spreadhseet
-# sheet = client.open("Commilito Backend").worksheet("commilito_backend")
-
-# # Get a list of all records
-# data = sheet.get_all_records()
-# pprint(data)
-
-# public_gsheets_url = "https://docs.google.com/spreadsheets/d/1tYPHXibwW4lMQBH8iaXOXqZHg5P7-lVQ7JZ8EGmki5w/edit#gid=0"
-# Create a connection object.
-
-rowcount = 4
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-# @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     return rows
-
-# sheet_url = st.secrets["public_gsheets_url"]
-# rows = run_query(f'SELECT * FROM "{sheet_url}"')
-# @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     return rows
-# def addUser(fname, lname):
-#     global rowcount
-#     rowcount += 1
-#     sheet.insert_row([str(rowcount-1), fname, lname], rowcount)
-
-
-# def addCourses(classes):
-#     global rowcount
-#     record = sheet.row_values(rowcount)
-#     sheet.update_cell(rowcount, 4, classes)
-
-
-# def favoriteSpace(space):
-#     global rowcount
-#     sheet.update_cell(rowcount, 5, space)
-
-
-# def phone(phone):
-#     global rowcount
-#     sheet.update_cell(rowcount, 6, phone)
-
 
 
 #START OF STREAMLIT CODE..................
